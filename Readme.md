5aeabe5706a6d1cb0f1585519d6ea46ff28626bc
Barry Tarlton <btarlton@creationmuseum.org>
2023-01-04 16:15:35 -0500

    throw exception if super small

--- Readme.md
+++ Readme.md
@@ -7,6 +7,20 @@
 docker run -it --mount type=bind,src=%cd%,target=/tmp --network=kafka-nw edenhill/kcat:1.7.1 -b kafka:9092 -P -t input-topic /tmp/order1.json
 ```
 
+## Useful Kafka Commands:
+###### These examples are running from local kafka install
+```CMD
+kafka-topics --list --bootstrap-server=localhost:9092
+
+kafka-topics --delete --topic input-topic --bootstrap-server=localhost:9092
+
+kafka-consumer-groups.bat --list --bootstrap-server=localhost:9092
+
+kafka-consumer-groups.bat --describe --group pizza-order-app --bootstrap-server=localhost:9092
+
+```
+
+
 
 
 ## Running Kafdrop
--- src/main/java/com/barry/kafkaconsumer/consumers/OrderConsumer.java
+++ src/main/java/com/barry/kafkaconsumer/consumers/OrderConsumer.java
@@ -11,10 +11,14 @@ import com.barry.kafkaconsumer.entities.PizzaOrderEvent;
 public class OrderConsumer {
 
     @KafkaListener(topics = "input-topic", groupId = "pizza-order-app")    
-    public void receiveOrderMessage(ConsumerRecord<String, PizzaOrderEvent> orderMessage){
+    public void receiveOrderMessage(ConsumerRecord<String, PizzaOrderEvent> orderMessage) throws Exception{
         System.out.println("Partition:" + orderMessage.partition());
-        System.out.println("Partition:" + orderMessage.offset());
+        System.out.println("Offset:" + orderMessage.offset());
         PizzaOrderEvent event = orderMessage.value();
+        System.out.println("Pizza Size:" + event.getSize());
+        if("SUPER SMALL".equalsIgnoreCase(event.getSize())){
+            throw new Exception("No stinking Super Small Pizzas allowed");
+        }
         System.out.println("Order Message Value:" + event);
     }
 }
